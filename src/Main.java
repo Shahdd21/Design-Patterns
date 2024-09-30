8502c3ec93998abe801a46ca01c36c5ae6d51d8a
Shahdd21 <88803131+Shahdd21@users.noreply.github.com>
2024-09-30 21:40:46 +0300

    template exercises

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
-import template_exercise_01.*;
+import template_exercise_02.*;
 
 public class Main {
     public static void main(String[] args) {
+        String message = "This is plain text";
 
+        Formatter json = new JsonFormatter();
+        json.format(message);
+
+        Formatter xml = new XmlFormatter();
+        xml.format(message);
     }
 }
---
+++ src/template_exercise_02/JsonFormatter.java
@@ -0,0 +1,8 @@
+package template_exercise_02;
+
+public class JsonFormatter extends Formatter{
+    @Override
+    protected String formatttedMessage(String message) {
+        return message+" formatted using JSON";
+    }
+}
